574659b70347845befa350ebb6871a58c0cb1b42
vagrant <vagrant@ubuntu-server>
2024-09-02 01:02:31 +0000

    pus

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,4 +10,4 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-chmod a+x bin/render-build.shbundle exec rails db:migrate
+bundle exec rails db:migrate
